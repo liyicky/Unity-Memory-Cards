47fe17a33851c3e92bd10787b4e9f93d032e0c00
liyicky <jason@liyicky.com>
2023-02-09 23:25:04 +0900

    init

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] GameObject cardBack;
+    private void OnMouseDown() {
+        Debug.Log("Liyicky was here");
+    }
+}
